14dd5f21725f78b700a9411b0c89b44aa18ef491
maks-march <98648053+maks-march@users.noreply.github.com>
2024-11-09 22:34:42 +0500

    changes

--- js/index_script.js
+++ js/index_script.js
@@ -1,3 +1,12 @@
+<<<<<<< Updated upstream
+=======
+// Функция для обработки загрузки файлов
+document.getElementById('file_input').addEventListener('change', function(event) {
+  const files = event.target.files;
+  console.log('Файлы загружены:', files);
+  // Здесь можно добавить логику для обработки файлов
+});
+>>>>>>> Stashed changes
 
 
 var result = location.search.substring(1,9);
